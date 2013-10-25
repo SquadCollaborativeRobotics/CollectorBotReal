93849c39f0e358175af09de7e18bb23d962d4b05
Alex Sher <alexbsher@gmail.com>
2013-10-25 13:10:28 -0400

    Factored Base Controller and made math for differential drive work.

--- src/base_controller_node.cpp
+++ src/base_controller_node.cpp
@@ -5,11 +5,12 @@
 #include <eigen3/Eigen/Dense>
 
 // Physical Parameters... What do to with these?
-float wheel_radius=4.875/2.0*.0254, axle_half_length=8.4*.0254;
-float motor_max_speed=2.5;
+double wheel_radius=4.875/2.0*.0254, axle_half_length=8.4*.0254;
+// Maximum loaded motor rotation speed in radians per sec
+double motor_max_speed=25;
 
 // Global values for velocity storage
-float cmd_vx=0, cmd_vy=0, cmd_vz=0, cmd_wx=0, cmd_wy=0, cmd_wz=0;
+double cmd_vx=0, cmd_vy=0, cmd_vz=0, cmd_wx=0, cmd_wy=0, cmd_wz=0;
 
 // Callback to attach to command velocity subscription
 void commandCallback(const geometry_msgs::Twist::ConstPtr& msg){
@@ -18,6 +19,31 @@ void commandCallback(const geometry_msgs::Twist::ConstPtr& msg){
   cmd_vx = msg->linear.x;
   cmd_wz = msg->angular.z;
  
+}
+
+// Function to calculate wheel speeds given a diff drive command
+// Models problem as Ax = b form
+Eigen::Vector2d getWheelSpeeds(double wheel_offset, double cmd_x, double cmd_th){
+  /*
+  @ Params:  wheel_offset -> Distance between center of wheel axle and center of wheel contact
+             cmd_x        -> Commanded forward velocity for the robot
+             cmd_th       -> Commanded angular velocity about the reference point
+  @ Returns: x            -> Eigen::Vector containing body velocities at wheel;
+                             x(0) -> Right Wheel
+                             x(1) -> Left Wheel
+  */
+
+  // Diff Drive Dynamics Solution, vr = right wheel velocity, vl = left wheel velocity
+  double vr = cmd_x + cmd_th * wheel_offset/2.0;
+  double vl = cmd_x - cmd_th * wheel_offset/2.0;
+
+  // Vector for result and return
+  Eigen::Vector2d x;
+  // Pack Vector with values and return
+  x(0) = vr;
+  x(1) = vl;
+  return x;
+
 }
 
 int main(int argc, char **argv){
@@ -37,27 +63,16 @@ int main(int argc, char **argv){
 
   while(ros::ok()){
     
-    // Calculate Motor Commands given command velocity
-    // A matrix based on body parameters
-    Eigen::Matrix2f A;
-    A << 1.0, 1.0,
-         1.0/(axle_half_length), -1.0/(axle_half_length);
-    
-    // b vector for command velocities
-    Eigen::Vector2f b;
-    b << cmd_vx,
-         cmd_wz;
-    
-    // x vector for storing wheel velocities after solve 
-    Eigen::Vector2f x;
+    // Vector for holding the linear speeds at the wheels
+    Eigen::Vector2d wheel_linear_speeds;
     
     // Pass to linear Alg to solver or something?
-    x = A.ldlt().solve(b);
+    wheel_linear_speeds = getWheelSpeeds(axle_half_length, cmd_vx, cmd_wz);
 
     // Convert Wheel Velocities to Angular Velocities
     // Be careful for sign flip, as motors are not mounted symmetrically
-    float right_wheel_omega = x(0)/wheel_radius;
-    float left_wheel_omega = x(1)/wheel_radius;
+    double right_wheel_omega = wheel_linear_speeds(0)/wheel_radius;
+    double left_wheel_omega = wheel_linear_speeds(1)/wheel_radius;
 
     // Log info to see output of linear system
     ROS_INFO("Right Motor Desired Speed = [%f]", right_wheel_omega);
@@ -67,9 +82,9 @@ int main(int argc, char **argv){
     scr_proto::DiffCommand motor_com;
 
     // Populate Message
-    // Need to map omega's to value between -255 and 255.
-    right_wheel_omega *= (255.0/motor_max_speed);
-    left_wheel_omega *= (255.0/motor_max_speed);
+    // Need to map omega's to value between -127 and 127 for motor driver
+    right_wheel_omega *= (127.0/motor_max_speed);
+    left_wheel_omega *= (127.0/motor_max_speed);
 
     motor_com.left_motor = (int)left_wheel_omega;
     motor_com.right_motor = (int)right_wheel_omega;
