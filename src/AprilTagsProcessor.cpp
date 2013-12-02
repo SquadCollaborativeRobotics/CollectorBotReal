f6654ab0e0bde1b54cc455facf09fa70fb92c0b6
Shawn Hanna <shawnkarlhanna@gmail.com>
2013-12-02 12:51:47 -0500

    Finished the transformation functions in the april tags processor

--- src/AprilTagsProcessor.cpp
+++ src/AprilTagsProcessor.cpp
@@ -6,7 +6,6 @@
 
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <std_msgs/Float64.h>
-//#include <tf/Vector3.h>
 
 #include <math.h>
 
@@ -15,7 +14,7 @@ std::vector<std::string> april_frames;
 
 // Publisher that sends out an april tag that is a possible goal node?
 ros::Publisher tags_pub;
-// Publisher that reinitializes the pose of the robot (relocalizes)
+// Publisher that publishes a pose for visualization purposes to the "/new_pose"
 ros::Publisher new_pose_pub;
 // Publisher that reinitializes the pose of the robot (relocalizes)
 ros::Publisher new_initial_pose_pub;
@@ -32,114 +31,101 @@ void init(ros::NodeHandle nh)
   new_initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 100);
 }
 
-
 bool AprilTagLocalize(tf::TransformListener &listener)
 {
-  for (int i = 0; i < landmark_frames.size(); i++)
+  static ros::Time lastPoseUpdateTime = ros::Time::now();
+
+  if (ros::Time::now() - lastPoseUpdateTime > ros::Duration(5.1))
   {
-    tf::StampedTransform mapLandmarkTransform;
-    tf::StampedTransform differenceTransform;
-    tf::StampedTransform tagToBaseTransform;
-    try{
-      if (listener.canTransform (landmark_frames[i], april_frames[i], ros::Time(0)))
-      {
-        listener.lookupTransform(april_frames[i], "/base_link",
-                                 ros::Time(0), tagToBaseTransform);
-        listener.lookupTransform(landmark_frames[i], april_frames[i],
-                                ros::Time(0), differenceTransform);
-        listener.lookupTransform("map", landmark_frames[i],
-                                 ros::Time(0), mapLandmarkTransform);
+    for (int i = 0; i < landmark_frames.size(); i++)
+    {
+      try{
+        // if it can find a transformation between a landmark frame and april tag frame (i.e. both are in the tree)
+        // watch out for tags still in tree but not seen in the camera
+        if (listener.canTransform (landmark_frames[i], april_frames[i], ros::Time(0)))
+        {
+          tf::StampedTransform mapLandmarkTransform;
+          tf::StampedTransform differenceTransform;
+          tf::StampedTransform tagToBaseTransform;
 
-        // ROS_INFO("Found transform for tag %s:\ndx = %lf, dy = %lf, dz = %lf", april_frames[i].c_str(),
-        //   differenceTransform.getOrigin().x(), differenceTransform.getOrigin().y(), differenceTransform.getOrigin().z());
+          listener.lookupTransform(april_frames[i], "/base_link",
+          ros::Time(0), tagToBaseTransform);
+          listener.lookupTransform("map", landmark_frames[i],
+          ros::Time(0), mapLandmarkTransform);
+          listener.lookupTransform(landmark_frames[i], april_frames[i],
+          ros::Time(0), differenceTransform);
 
+          if (ros::Time::now() - tagToBaseTransform.stamp_ < ros::Duration(1.0))
+          {
 
-        //Create a pose based in the /map frame
-        geometry_msgs::PoseWithCovariance poseWithCovariance;
-        geometry_msgs::Pose pose;
+            //Create a pose based in the /map frame
+            geometry_msgs::PoseWithCovariance poseWithCovariance;
+            geometry_msgs::Pose pose;
 
-        tf::Transform tf;
-        tf.setOrigin(tf::Vector3(mapLandmarkTransform.getOrigin().x() + tagToBaseTransform.getOrigin().x(),
-          mapLandmarkTransform.getOrigin().y() + tagToBaseTransform.getOrigin().y(),
-          0));
+            tf::Vector3 lm_vec = mapLandmarkTransform.getOrigin();
+            tf::Vector3 april_vec = tagToBaseTransform.getOrigin();
+            tf::Matrix3x3 rot_vec = mapLandmarkTransform.getBasis();
 
-        // tf.setRotation(mapLandmarkTransform.getRotation().x() + tagToBaseTransform.getRotation().x(),
-        //   mapLandmarkTransform.getRotation().y() + tagToBaseTransform.getRotation().y(),
-        //   mapLandmarkTransform.getRotation().z() + tagToBaseTransform.getRotation().z());
+            // std::cout << tagToBaseTransform.getOrigin().x() <<" "<< tagToBaseTransform.getOrigin().y() <<" "<< tagToBaseTransform.getOrigin().z() <<" "<< std::endl;
+            // std::cout << mapLandmarkTransform.getOrigin().x() <<" "<< mapLandmarkTransform.getOrigin().y() <<" "<< mapLandmarkTransform.getOrigin().z() <<" "<< std::endl;
 
-        std::cout << getYaw(mapLandmarkTransform.getRotation())<<std::endl;
+            tf::Transform tf;
+            tf.setOrigin(lm_vec + (april_vec * rot_vec));
 
-        // tf.setRotation(tf::createQuaternionFromYaw(atan2(tagToBaseTransform.getOrigin().y(),
-        //                                                  tagToBaseTransform.getOrigin().x()) - getYaw(mapLandmarkTransform.getRotation())));
+            double yawMap = getYaw(mapLandmarkTransform.getRotation());
+            double yawTag = getYaw(tagToBaseTransform.getRotation());
 
-        double rotationDueToXY = atan2(tagToBaseTransform.inverse().getOrigin().y(),tagToBaseTransform.inverse().getOrigin().x());
-        tf.setRotation(tf::createQuaternionFromYaw(getYaw(mapLandmarkTransform.getRotation()) + rotationDueToXY));
+            tf.setRotation(tf::createQuaternionFromYaw(yawMap + yawTag));
 
-        // tf.setRotation(tf::createQuaternionFromYaw(differenceTransform.getRotation().z()));
+            geometry_msgs::Transform msg_tf;
+            tf::transformTFToMsg(tf, msg_tf);
 
-        geometry_msgs::Transform msg_tf;
-        tf::transformTFToMsg(tf, msg_tf);
+            pose.position.x = msg_tf.translation.x;
+            pose.position.y = msg_tf.translation.y;
+            pose.position.z = 0;
+            pose.orientation = msg_tf.rotation;
 
-        /*
-        pose.position.x = mapLandmarkTransform.getOrigin().x() + tagToBaseTransform.getOrigin().x();
-        pose.position.y = mapLandmarkTransform.getOrigin().y() + tagToBaseTransform.getOrigin().y();
-        pose.position.z = 0;//mapLandmarkTransform.getOrigin().z() - differenceTransform.getOrigin().z() + tagToBaseTransform.getOrigin().z();
+            ROS_INFO("New Robot Position:\ndx = %lf, dy = %lf, dz = %lf",
+            pose.position.x, pose.position.y, pose.position.z);
 
-        // Quaternion for the rotation transform between the landmark and april tag's pose
-        geometry_msgs::Quaternion q;
-        q.x = mapLandmarkTransform.getRotation().x() + tagToBaseTransform.getRotation().x();
-        q.y = mapLandmarkTransform.getRotation().y() + tagToBaseTransform.getRotation().y();
-        q.z = mapLandmarkTransform.getRotation().z() + tagToBaseTransform.getRotation().z();
+            geometry_msgs::PoseWithCovarianceStamped newRobotPose;
+            newRobotPose.header.frame_id = "map";
+            newRobotPose.header.stamp = tagToBaseTransform.stamp_;
 
-        pose.orientation = q;
-        */
+            poseWithCovariance.pose = pose;
 
-        pose.position.x = msg_tf.translation.x;
-        pose.position.y = msg_tf.translation.y;
-        pose.position.z = msg_tf.translation.z;
-        pose.orientation = msg_tf.rotation;
+            // create the covariance array (the values are based on what rviz sends)
+            // TODO: change based on confidence (distance to tag?)
+            boost::array<double, 36> covariance = {0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942};
 
-        ROS_INFO("New Position:\ndx = %lf, dy = %lf, dz = %lf",
-          pose.position.x, pose.position.y, pose.position.z);
+            poseWithCovariance.covariance = covariance;
+            newRobotPose.pose = poseWithCovariance;
 
-        /*
-        ROS_INFO("New Quaternion:\ndx = %lf, dy = %lf, dz = %lf",
-          q.x, q.y, q.z);
-        */
+            geometry_msgs::PoseStamped poseStamped;
+            poseStamped.header.frame_id = "map";
+            poseStamped.header.stamp = ros::Time::now();
+            poseStamped.pose = pose;
 
-        geometry_msgs::PoseWithCovarianceStamped newRobotPose;
-        newRobotPose.header.frame_id = "map";
-        newRobotPose.header.stamp = differenceTransform.stamp_;
+            new_pose_pub.publish(poseStamped);
+            new_initial_pose_pub.publish(newRobotPose);
 
-        poseWithCovariance.pose = pose;
-
-        //create the covariance array (the values are based on what rviz sends)
-        boost::array<double, 36> covariance = {0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942};
-
-        poseWithCovariance.covariance = covariance;
-        newRobotPose.pose = poseWithCovariance;
-
-        geometry_msgs::PoseStamped poseStamped;
-        poseStamped.header.frame_id = "map";
-        poseStamped.header.stamp = ros::Time::now();
-        poseStamped.pose = pose;
-
-        new_pose_pub.publish(poseStamped);
-        new_initial_pose_pub.publish(newRobotPose);
+            lastPoseUpdateTime = ros::Time::now();
+          }
+        }
       }
-    }
-    catch (tf::TransformException ex){
-      ROS_INFO("could not transform from %s to %s", landmark_frames[i].c_str(), april_frames[i].c_str());
-      return false;
+      catch (tf::TransformException ex){
+        ROS_INFO("could not transform from %s to %s", landmark_frames[i].c_str(), april_frames[i].c_str());
+        return false;
+      }
     }
   }
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "odometry_publisher");
+  ros::init(argc, argv, "AprilTagsProcessor_node");
 
   ros::NodeHandle nh;
 
@@ -170,7 +156,7 @@ int main(int argc, char **argv)
       if (listener.canTransform ("/map", ss.str().c_str(), ros::Time(0)))
       {
         listener.lookupTransform("/map", ss.str().c_str(),
-                                 ros::Time(0), transform);
+         ros::Time(0), transform);
         quat = transform.getRotation();
 
         char buf[2];
@@ -178,6 +164,7 @@ int main(int argc, char **argv)
         ps.pose.position.x = transform.getOrigin().x();
         ps.pose.position.y = transform.getOrigin().y();
         ps.pose.position.z = transform.getOrigin().z();
+
         geometry_msgs::Quaternion q;
         q.x=quat.x();
         q.y=quat.y();
