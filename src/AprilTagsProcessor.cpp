064c1cc98ae24cf55597aabea18e858b4c5b6334
Shawn Hanna <shawnkarlhanna@gmail.com>
2013-12-02 09:22:12 -0500

    added static transforms launch file.  readded the prototype's odometrybroadcaster.  modified the apriltagsprocessor node

--- src/AprilTagsProcessor.cpp
+++ src/AprilTagsProcessor.cpp
@@ -5,24 +5,33 @@
 #include <stdio.h>
 
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <std_msgs/Float64.h>
+//#include <tf/Vector3.h>
 
-
-ros::NodeHandle nh;
+#include <math.h>
 
 std::vector<std::string> landmark_frames;
 std::vector<std::string> april_frames;
 
-void init()
+// Publisher that sends out an april tag that is a possible goal node?
+ros::Publisher tags_pub;
+// Publisher that reinitializes the pose of the robot (relocalizes)
+ros::Publisher new_pose_pub;
+// Publisher that reinitializes the pose of the robot (relocalizes)
+ros::Publisher new_initial_pose_pub;
+
+void init(ros::NodeHandle nh)
 {
   landmark_frames.push_back(std::string("/landmark_3"));
   landmark_frames.push_back(std::string("/landmark_5"));
   april_frames.push_back(std::string("/april_tag[3]"));
   april_frames.push_back(std::string("/april_tag[5]"));
+
+  tags_pub = nh.advertise<geometry_msgs::PoseStamped>("/apriltags", 100);
+  new_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("/new_pose", 100);
+  new_initial_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 100);
 }
 
-// Publisher that sends out an april tag that is a possible goal node
-ros::Publisher tags_pub = nh.advertise<geometry_msgs::PoseStamped>("/apriltags", 1000);
-ros::Publisher new_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1000);
 
 bool AprilTagLocalize(tf::TransformListener &listener)
 {
@@ -30,45 +39,95 @@ bool AprilTagLocalize(tf::TransformListener &listener)
   {
     tf::StampedTransform mapLandmarkTransform;
     tf::StampedTransform differenceTransform;
+    tf::StampedTransform tagToBaseTransform;
     try{
       if (listener.canTransform (landmark_frames[i], april_frames[i], ros::Time(0)))
       {
+        listener.lookupTransform(april_frames[i], "/base_link",
+                                 ros::Time(0), tagToBaseTransform);
         listener.lookupTransform(landmark_frames[i], april_frames[i],
-                                 ros::Time(0), differenceTransform);
-        listener.lookupTransform("/map", landmark_frames[i],
+                                ros::Time(0), differenceTransform);
+        listener.lookupTransform("map", landmark_frames[i],
                                  ros::Time(0), mapLandmarkTransform);
 
-        tf::Quaternion differenceQuat = differenceTransform.getRotation();
-        tf::Quaternion mapLandmarkQuat = mapLandmarkTransform.getRotation();
-
-        ROS_INFO("Found transform for tag %d:\ndx = %lf, dy = %lf, dz = %lf", i,
-          differenceTransform.getOrigin().x(), differenceTransform.getOrigin().y(), differenceTransform.getOrigin().z());
+        // ROS_INFO("Found transform for tag %s:\ndx = %lf, dy = %lf, dz = %lf", april_frames[i].c_str(),
+        //   differenceTransform.getOrigin().x(), differenceTransform.getOrigin().y(), differenceTransform.getOrigin().z());
 
 
         //Create a pose based in the /map frame
         geometry_msgs::PoseWithCovariance poseWithCovariance;
         geometry_msgs::Pose pose;
 
-        pose.position.x = mapLandmarkTransform.getOrigin().x() - differenceTransform.getOrigin().x();
-        pose.position.y = mapLandmarkTransform.getOrigin().y() - differenceTransform.getOrigin().y();
-        pose.position.z = mapLandmarkTransform.getOrigin().z() - differenceTransform.getOrigin().z();
+        tf::Transform tf;
+        tf.setOrigin(tf::Vector3(mapLandmarkTransform.getOrigin().x() + tagToBaseTransform.getOrigin().x(),
+          mapLandmarkTransform.getOrigin().y() + tagToBaseTransform.getOrigin().y(),
+          0));
+
+        // tf.setRotation(mapLandmarkTransform.getRotation().x() + tagToBaseTransform.getRotation().x(),
+        //   mapLandmarkTransform.getRotation().y() + tagToBaseTransform.getRotation().y(),
+        //   mapLandmarkTransform.getRotation().z() + tagToBaseTransform.getRotation().z());
+
+        std::cout << getYaw(mapLandmarkTransform.getRotation())<<std::endl;
+
+        // tf.setRotation(tf::createQuaternionFromYaw(atan2(tagToBaseTransform.getOrigin().y(),
+        //                                                  tagToBaseTransform.getOrigin().x()) - getYaw(mapLandmarkTransform.getRotation())));
+
+        double rotationDueToXY = atan2(tagToBaseTransform.inverse().getOrigin().y(),tagToBaseTransform.inverse().getOrigin().x());
+        tf.setRotation(tf::createQuaternionFromYaw(getYaw(mapLandmarkTransform.getRotation()) + rotationDueToXY));
+
+        // tf.setRotation(tf::createQuaternionFromYaw(differenceTransform.getRotation().z()));
+
+        geometry_msgs::Transform msg_tf;
+        tf::transformTFToMsg(tf, msg_tf);
+
+        /*
+        pose.position.x = mapLandmarkTransform.getOrigin().x() + tagToBaseTransform.getOrigin().x();
+        pose.position.y = mapLandmarkTransform.getOrigin().y() + tagToBaseTransform.getOrigin().y();
+        pose.position.z = 0;//mapLandmarkTransform.getOrigin().z() - differenceTransform.getOrigin().z() + tagToBaseTransform.getOrigin().z();
 
         // Quaternion for the rotation transform between the landmark and april tag's pose
         geometry_msgs::Quaternion q;
-        q.x = mapLandmarkQuat.x() - differenceQuat.x();
-        q.y = mapLandmarkQuat.y() - differenceQuat.y();
-        q.z = mapLandmarkQuat.z() - differenceQuat.z();
+        q.x = mapLandmarkTransform.getRotation().x() + tagToBaseTransform.getRotation().x();
+        q.y = mapLandmarkTransform.getRotation().y() + tagToBaseTransform.getRotation().y();
+        q.z = mapLandmarkTransform.getRotation().z() + tagToBaseTransform.getRotation().z();
 
         pose.orientation = q;
+        */
+
+        pose.position.x = msg_tf.translation.x;
+        pose.position.y = msg_tf.translation.y;
+        pose.position.z = msg_tf.translation.z;
+        pose.orientation = msg_tf.rotation;
+
+        ROS_INFO("New Position:\ndx = %lf, dy = %lf, dz = %lf",
+          pose.position.x, pose.position.y, pose.position.z);
+
+        /*
+        ROS_INFO("New Quaternion:\ndx = %lf, dy = %lf, dz = %lf",
+          q.x, q.y, q.z);
+        */
 
         geometry_msgs::PoseWithCovarianceStamped newRobotPose;
-        newRobotPose.header.frame_id = "/map";
+        newRobotPose.header.frame_id = "map";
         newRobotPose.header.stamp = differenceTransform.stamp_;
 
         poseWithCovariance.pose = pose;
+
+        //create the covariance array (the values are based on what rviz sends)
+        boost::array<double, 36> covariance = {0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942};
+
+        poseWithCovariance.covariance = covariance;
         newRobotPose.pose = poseWithCovariance;
 
-        new_pose_pub.publish(newRobotPose);
+        geometry_msgs::PoseStamped poseStamped;
+        poseStamped.header.frame_id = "map";
+        poseStamped.header.stamp = ros::Time::now();
+        poseStamped.pose = pose;
+
+        new_pose_pub.publish(poseStamped);
+        new_initial_pose_pub.publish(newRobotPose);
       }
     }
     catch (tf::TransformException ex){
@@ -82,17 +141,21 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "odometry_publisher");
 
+  ros::NodeHandle nh;
+
   tf::TransformListener listener(nh);
 
-  init();
+  init(nh);
 
   // Initialize variables for loop
   ros::Rate r(10.0);
 
   while(nh.ok()){
 
-    AprilTagLocalize(listener);
+    // Update callbacks
+    ros::spinOnce();
 
+    AprilTagLocalize(listener);
 
     //get the pose of the robot in the map frame and publish
     tf::StampedTransform transform; //transform from landmark to where the camera sees the april tag
@@ -114,6 +177,7 @@ int main(int argc, char **argv)
         ps.header.frame_id = sprintf(buf, "%d", i);
         ps.pose.position.x = transform.getOrigin().x();
         ps.pose.position.y = transform.getOrigin().y();
+        ps.pose.position.z = transform.getOrigin().z();
         geometry_msgs::Quaternion q;
         q.x=quat.x();
         q.y=quat.y();
@@ -123,8 +187,6 @@ int main(int argc, char **argv)
       }
     }
 
-    // Update callbacks after the fact, for next loop iteration.
-    ros::spinOnce();
     r.sleep();
   }
 }
--- src/OdometryBroadcaster.cpp
+++ src/OdometryBroadcaster.cpp
@@ -1,9 +1,21 @@
+// http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
+
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <nav_msgs/Odometry.h>
+#include "std_msgs/Float32.h"
+
+// State variables
+double x = 0.0;
+double y = 0.0;
+double th = 0.0;
+
+double vx = 0.0;
+double vy = 0.0;
+double vth = 0.0;
+
+double lws = 0; // Left wheel speed (angular velocity)
+double rws = 0; // Right wheel speed (angular velocity)
 
 #define PI 3.1415926
 
@@ -15,29 +27,123 @@
 // the numbers here are: 0.445 => distance between the far edge of each wheel
 //                   0.01 * 2 => 2 cm wheels, so the distance is 0.01*2
 
+// Last time received callback for left or right wheel.
+ros::Time last_lw_time, last_rw_time;
+
+bool hasNewSpeeds = true;
+
+// Update current left wheel speed
+void lw_speed_callback(const std_msgs::Float32::ConstPtr& msg)
+{
+  last_lw_time = ros::Time::now();
+  //ROS_INFO("Left wheel speed: [%lf]", msg->data);
+  // NOTE : Left wheel speed is opposite because motor is reversed on axis
+  lws = -msg->data;
+}
+
+// Update current right wheel speed
+void rw_speed_callback(const std_msgs::Float32::ConstPtr& msg)
+{
+  last_rw_time = ros::Time::now();
+  //ROS_INFO("Right wheel speed: [%lf]", msg->data);
+  rws = msg->data;
+}
+
 
 int main(int argc, char** argv){
-  ros::init(argc, argv, "odometry_publisher");
+  ros::init(argc, argv, "odometry_broadcaster");
 
   ros::NodeHandle n;
 
-  tf::TransformListener listener(n);
-  
+  // Subscribers
+  ros::Subscriber lw_sub = n.subscribe("lw_speed", 10, lw_speed_callback);
+  ros::Subscriber rw_sub = n.subscribe("rw_speed", 10, rw_speed_callback);
+
   // Publishers
+  ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 1);
 
   // Transform Broadcasters
-  tf::TransformBroadcaster camera_link_broadcaster;
+  tf::TransformBroadcaster odom_broadcaster;
 
   // Initialize variables for loop
-  ros::Rate r(30.0);
+  ros::Rate r(60.0);
+  ros::Time current_time = ros::Time::now();
+  ros::Time last_time = ros::Time::now();
+
+  ros::Time lastPrintedTime = ros::Time::now();
 
   while(n.ok()){
-    // Broadcast the base link to kinect
-    camera_link_broadcaster.sendTransform(
-        tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0.0, 1),
-                      tf::Vector3(0.0, 0.0, 0.0762)),
-        ros::Time::now(),"base_link", "camera_link"));
+    // Set current time
+    current_time = ros::Time::now();
+
+    // Only update values if the last updated wheel speeds were within the last second, otherwise zero them out
+    if ((current_time - last_lw_time < ros::Duration(1.0)) && (current_time - last_rw_time < ros::Duration(1.0)))
+
+    {
+      //compute odometry in a typical way given the velocities of the robot
+      double dt = (current_time - last_time).toSec();
+
+      // Configuration Transition Equation - http://planning.cs.uiuc.edu/node659.html
+      // The lws and rws are in terms of radians/second
+      vx  = (WHEEL_RADIUS) * (lws + rws) / 2.0 * cos(th);
+      vy  = (WHEEL_RADIUS) * (lws + rws) / 2.0 * sin(th);
+      vth = (WHEEL_RADIUS / WHEEL_SEPARATION) * (rws - lws);
+
+      // Update positions with velocity integrated euler step
+      x  += vx * dt;
+      y  += vy * dt;
+      th += vth * dt;
+    }
+    else
+    {
+      if (ros::Time::now() - lastPrintedTime > ros::Duration(3.0))
+      {
+        ROS_ERROR("Have not received a wheel speed update in: %lf", (current_time-last_lw_time).toSec());
+        lastPrintedTime = ros::Time::now();
+      }
+
+      vx = vy = vth = 0;
+    }
+
+    // Update last time to now and rate limit loop speed
+    last_time = current_time;
+
+    // Since all odometry is 6DOF we'll need a quaternion created from yaw
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+    // First, we'll publish the transform over tf
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+
+    odom_trans.transform.translation.x = x;
+    odom_trans.transform.translation.y = y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.transform.rotation = odom_quat;
+
+    // Send the transform
+    odom_broadcaster.sendTransform(odom_trans);
+
+    // Next, we'll publish the odometry message over ROS
+    nav_msgs::Odometry odom;
+    odom.header.stamp = current_time;
+    odom.header.frame_id = "odom";
+
+    // Set the position
+    odom.pose.pose.position.x = x;
+    odom.pose.pose.position.y = y;
+    odom.pose.pose.position.z = 0.0;
+    odom.pose.pose.orientation = odom_quat;
+
+    // Set the velocity
+    odom.child_frame_id = "base_link";
+    odom.twist.twist.linear.x = vx;
+    odom.twist.twist.linear.y = vy;
+    odom.twist.twist.angular.z = vth;
+
+    // Publish the message
+    odom_pub.publish(odom);
 
     // Update callbacks after the fact, for next loop iteration.
     ros::spinOnce();
